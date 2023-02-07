321aae5c4652291b118cd85c76b47fb0f8f89945
achitsak028 <patong028@gmail.com>
2023-02-07 15:56:28 +0700

    lab16_1 complete

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,25 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+int a=5;
+char b='A';
+char &c=b;
+int *x,*z;
+char *y;
+x = &a;
+y = &b;
+z = x;
+cout << a << " " << b << " " << c << " " << x << " " << (int *)y << " " << z << endl;
+	cout << &a << " " << (int *)&b << " " << (int *)&c << " " << &x << " " << (int *)&y << " " << &z << endl;
+	b ='F';
+	cout << a << " " << b << " " << c << " " << x << " " << (int *)y << " " << z << endl;
+	*y ='W';
+    cout << a << " " << b << " " << c << " " << x << " " << (int *)y << " " << z << endl;
+	*x = 6;
+	cout << a << " " << b << " " << c << " " << x << " " << (int *)y << " " << z << endl;
+	*z = 7;
+	cout << a << " " << b << " " << c << " " << x << " " << (int *)y << " " << z;
 
-	
-	
 	return 0;
 }
